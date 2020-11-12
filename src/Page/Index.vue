e25d1ee19863f7b70d7fcf4da5e2c76be34d3232
unknown <2281254643@qq.com>
2020-11-12 15:56:04 +0800

    前端18陈杨项目

---
+++ src/Page/Index.vue
@@ -0,0 +1,298 @@
+<template>
+    <div>
+
+            <div class="dingbuys">
+               
+            </div>
+            
+            <div class="ShoY">
+                <span class="el-icon-s-home"><a href="#"> 首页</a></span>
+            </div>
+
+                <div class="mnue">
+                    <div class="yid">
+            <div class="mnue-tit">
+			        <a href="#"><span><img style="width:25px" src="../image/touxiang.png"></span><span>用户昵称</span></a>
+			        <em></em>
+			        <div class="mnue-son">
+				<ul>
+					<li>
+						<a href="#">修改密码</a>
+					</li>
+					<li>
+						<a href="#">退出</a>
+					</li>
+				</ul>
+			</div>
+		</div>	
+       </div>
+	</div>
+    <div class="wenbena">
+        <p>会员管理>会员列表</p>
+    </div>
+    <div class="wenben">
+        <div style="margin-top: 15px;">
+  <el-input placeholder="请输入搜索内容" v-model="input3" class="input-with-select">
+    <el-select v-model="select" slot="prepend" placeholder="请选择">
+      <el-option label="姓名" value="1"></el-option>
+      <el-option label="昵称" value="2"></el-option>
+      <el-option label="手机号" value="3"></el-option>
+      <el-option label="会员卡号" value="4"></el-option>
+    </el-select>
+    <el-button slot="append" icon="el-icon-search"></el-button>
+  </el-input>
+</div>
+    </div>
+    
+    <div class="daohang">
+<el-col :span="12">
+    <el-menu
+      default-active="2"
+      class="el-menu-vertical-demo"
+      @open="handleOpen"
+      @close="handleClose"
+      background-color="#545c64"
+      text-color="#fff"
+      active-text-color="#ffd04b">
+      <el-submenu index="1">
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span>导航栏</span>
+        </template>
+        <el-menu-item-group>
+          <el-menu-item index="1-1">会员列表</el-menu-item>
+          <el-menu-item index="1-2">充值设置</el-menu-item>
+           <el-menu-item index="1-1">积分设置</el-menu-item>
+          <el-menu-item index="1-2">充值记录</el-menu-item>
+           <el-menu-item index="1-1">消费记录</el-menu-item>
+          <el-menu-item index="1-2">优惠券管理</el-menu-item>
+           <el-menu-item index="1-1">商品列表</el-menu-item>
+          <el-menu-item index="1-2">商品分类</el-menu-item>
+           <el-menu-item index="1-1">商品订单</el-menu-item>
+          <el-menu-item index="1-2">客房订单</el-menu-item>
+           <el-menu-item index="1-1">住宿记录</el-menu-item>
+          <el-menu-item index="1-2">房态管理</el-menu-item>
+           <el-menu-item index="1-1">房型管理</el-menu-item>
+          <el-menu-item index="1-2">房号管理</el-menu-item>
+           <el-menu-item index="1-1">投诉建议</el-menu-item>
+          <el-menu-item index="1-2">管理员列表</el-menu-item>
+           <el-menu-item index="1-1">角色管理</el-menu-item>
+          <el-menu-item index="1-2">微信设置</el-menu-item>
+           <el-menu-item index="1-1">酒店设置</el-menu-item>
+          <el-menu-item index="1-2">其它设置</el-menu-item>
+        </el-menu-item-group>
+      </el-submenu>
+    </el-menu>
+  </el-col>
+    </div>
+    <div class="wenbena">
+        <p>会员管理>会员列表</p>
+    </div>
+    <div class="biaodan">
+         <el-table
+    ref="multipleTable"
+    :data="tableData"
+    tooltip-effect="dark"
+    style="width: 100%"
+    @selection-change="handleSelectionChange">
+    <el-table-column
+      type="selection"
+      width="55">
+    </el-table-column>
+    <el-table-column
+      label="用户信息"
+      width="120">
+      <template slot-scope="scope">{{ scope.row.date }}</template>
+    </el-table-column>
+    <el-table-column
+      prop="name"
+      label="手机号码"
+      width="120">
+    </el-table-column>
+    <el-table-column
+      prop="address"
+      label="可用余额"
+      show-overflow-tooltip>
+    </el-table-column>
+    <el-table-column
+      prop="jifen"
+      label="可用积分"
+      show-overflow-tooltip>
+    </el-table-column>
+    <el-table-column
+      prop="caozuo"
+      label="操作"
+      show-overflow-tooltip>
+    </el-table-column>
+  </el-table>
+ 
+    </div>
+    <div class="fenye">
+        <div class="block">
+    <span class="demonstration"></span>
+    <el-pagination
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :current-page="currentPage4"
+      :page-sizes="[100, 200, 300, 400]"
+      :page-size="100"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="400">
+    </el-pagination>
+  </div>
+    </div>
+    </div>
+    
+</template>
+<script>
+export default {
+  data() {
+    return {
+      input1: '',
+      input2: '',
+      input3: '',
+      select: '',
+    }
+  }
+}
+</script>
+<script>
+export default {
+     data() {
+      return {
+        tableData: [{
+          date: '用户昵称-张三',
+          name: '12312312311',
+          address: '1000.00',
+          jifen:'789'
+        }, {
+          date: '用户昵称-张三',
+          name: '23456543211',
+          address: '900.00',
+          jifen:'789'
+        }, {
+          date: '用户昵称-张三',
+          name: '88765434567',
+          address: '5000.00',
+          jifen:'789'
+        }, {
+         date: '用户昵称-张三',
+          name: '33567832156',
+          address: '3000.00',
+          jifen:'789'
+        }, {
+         date: '用户昵称-张三',
+          name: '08778643211',
+          address: '200.00',
+          jifen:'789'
+        }, {
+         date: '用户昵称-张三',
+          name: '11245678789',
+          address: '100.00',
+          jifen:'789'
+        }, {
+         date: '用户昵称-张三',
+          name: '14532456678',
+          address: '1200.00',
+          jifen:'789'
+        }],
+        multipleSelection: []
+      }
+    },
+
+   
+  }
+
+</script>
+<style scoped>
+.fenye{
+    margin: 20px 350px 0px 0px;
+}
+.biaodan{
+    margin: -160px 0px 0px 230px;
+}
+
+ .el-select .el-input {
+    width: 130px;
+  }
+  .input-with-select .el-input-group__prepend {
+    background-color: #fff;
+  }
+
+.wenben{
+    margin: 20px 0px 0px 240px;
+    width: 300px;
+}
+
+.daohang{
+    width: 401px;
+    height: 300px;
+    margin-top: -98px;
+}
+
+.yid{
+    margin: -20px 0px 0px 1100px;
+}
+* {
+			margin: 0;
+			padding: 0;
+		}
+		ul {
+			list-style: none; 
+		}
+		a {
+			text-decoration: none; 
+			color: #666;
+		}
+	
+		.mnue-tit {
+			margin: 0;
+			width: 150px;
+			height: 29px;
+			margin: 0 auto;
+		
+		}
+		.mnue-tit a span {
+			margin-left: 0px;
+		}
+		.mnue-son {
+			display: none;  /*隐藏下拉内容*/
+			width: 150px;
+			height: 65px;
+			padding-top: 5px;
+			background-color: #ddd;
+		}
+		.mnue-tit:hover .mnue-son {
+			display: block;
+		}
+		.mnue-son li {
+			text-align: center; /*显示下拉内容*/
+			padding-top: 10px;
+		}
+		.mnue-son li:hover a {
+			color: #e62310;
+		}
+.wenbena{
+    margin: 25px 0px 0px -700px;
+}
+.dingbuys{
+    margin-top: -70px;
+    width:100%;
+    height: 50px;
+    background: #ecebeb
+}
+.ShoY{
+    margin-left: -1200px;
+    margin-top: -35px;
+}
+a:link,a:visited{
+ text-decoration:none;
+}
+.nicheng{
+    margin: 0px -1100px 0px 0px; 
+}
+
+
+
+
+</style>
---
+++ src/Page/Login.vue
@@ -0,0 +1,52 @@
+<template>
+<div>
+    <div id="tupian1">
+      
+    </div>
+   <div class="logo">
+       <img style="width:250px" src="../image/logo.png">
+   </div>
+   <div class="shurukuang">
+  <el-input style="width:300px"
+placeholder="请输入内容"
+  v-model="input"
+  clearable>
+</el-input>
+<p></p>
+<el-input style="width:300px" placeholder="请输入密码" v-model="input1" show-password></el-input>
+<br/>
+<p></p>
+  <router-link tag="p" to="../Page/Index.vue"><el-button style="width:300px" type="success">登录</el-button></router-link>
+   </div>
+ 
+</div>
+
+</template>
+<script>
+export default {
+    data() {
+      return {
+        input: '',
+         input1: ''
+      }
+      
+    }
+    
+}
+</script>
+<style scoped>
+
+#tupian1{
+    height: 700px;
+    margin: -110px 0px 0px 0px;
+	background-image: url(../image/beijing.jpg);
+}
+.logo{
+    margin: -540px 0px 0px 30px
+}
+.shurukuang{
+    margin: 20px 0px 0px 40px;
+}
+    
+
+</style>
